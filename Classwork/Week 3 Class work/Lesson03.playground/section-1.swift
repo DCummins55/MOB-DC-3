e6241c351f81ab0e9787ddd90ee85e32e2290e39
Devin <DCummins55@gmail.com>
2015-06-10 21:27:45 -0400

    class 3

--- Classwork/Week 3 Class work/Lesson03.playground/section-1.swift
+++ Classwork/Week 3 Class work/Lesson03.playground/section-1.swift
@@ -4,10 +4,23 @@ import Foundation
 
 // TODO: Create two variables, name and age. Name is a string, age is an integer.
 
+let stringage = 28
+
+
+println("my name is Devin")
+
+let name = "Devin"
+
+
+
+
 // TODO: Print "Hello {whatever the value of name is}, you are {whatever the value of age is} years old!"
 
+println("Hello {Devin}, you are {28} years old")
 
 // TODO: Print “You can drink” below the above text if the user is above 21. If they are above 18, print “you can vote”. If they are above 16, print “You can drive”
+
+println("you can drink" >21 >)
 
 // TODO: Print “you can drive” if the user is above 16 but below 18. It should print “You can drive and vote” if the user is above 18 but below 21. If the user is above 21, it should print “you can drive, vote and drink (but not at the same time!”.
 
---
+++ Lesson 3 demo 2.playground/Contents.swift
@@ -0,0 +1,17 @@
+let name = "Devin"
+
+println("Devin")
+
+// f(g)
+
+println(name)
+
+var age = 28
+println(age)
+
+println("my name is \(name)")
+
+// /()
+
+
+
