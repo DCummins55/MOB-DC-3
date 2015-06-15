5393381fa40e3d0d29135de9b933652d617635d4
Devin <DCummins55@gmail.com>
2015-06-15 19:41:09 -0400

    Merging towards the new git structure

--- Classwork/lesson 3/Lesson03.playground/section-1.swift
+++ Classwork/lesson 3/Lesson03.playground/section-1.swift
@@ -64,25 +64,32 @@ let number = 5
 
 // TODO: Print whether the above number is even (not sure how do do even odd)
 
+// if calldnumber =
 
-while number == 1 {
-    println("odd")
-}
+
 // TODO: Print out "Hello {whatever the value of name is}, your name is {however long the string name is} characters long!. Use countElements()
 
 
 
 // TODO: Print the sum of one hundred random numbers. Use rand() to generate random numbers.
 
-var n = 1
-var sum: Int32 = 0
-while ( n < 100 ) {
-    sum = sum + Int(random())
-    println(sum)
-    ++N;
-}
+//var n = 1
+//var sum: Int32 = 0
+//while ( n < 100 ) {
+  //  sum = sum + Int(random())
+    //println(sum)
+    //++N;
+//}
 
 
 // Bonus TO DO: Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
 
-// BONUS TODO: The first fibonacci number is 0, the second is 1, the third is 1, the fourth is two, the fifth is 3, the sixth is 5, etc. The Xth fibonacci number is the sum of the X-1th fibonacci number and the X-2th fibonacci number. Print the 37th fibonacci number below
+//for i in 1...100 {
+    
+  //  if  i % 3 == 0 && i % 5 == 0 {
+    //    println()
+    //}
+// }
+
+
+// BONUS TODO: The first fibonacci number is 0, the second is 1, the third is 1, the fourth is two, the fifth is 3, the sixth is 5, etc. The Xth fibonacci number is the sum of the X-1th fibonacci number and the X-2th fibonacci number. Print the 37th fibonacci number beloW
